04e27f2390e2cda308f2092c4264a1f08120ecbd
GGA <41163962+geoffreygo@users.noreply.github.com>
2018-08-06 20:43:10 -0600

    mostly complete code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,9 @@ $(document).ready(function () {
         healthPoints: 120,
         baseAttackPower: 7,
         attackPower: 7,
-        counterAttack: 7,
-        hasLost: false,
         isEnemy: false,
         id: "#hironman",
+        hpid: "#ihp",
     };
 
     var hulk = {
@@ -20,10 +19,9 @@ $(document).ready(function () {
         healthPoints: 110,
         baseAttackPower: 8,
         attackPower: 8,
-        counterAttack: 8,
-        hasLost: false,
         isEnemy: false,
         id: "#hhulk",
+        hpid: "#hhp",
     };
 
     var ultron = {
@@ -33,10 +31,9 @@ $(document).ready(function () {
         healthPoints: 130,
         baseAttackPower: 6,
         attackPower: 6,
-        counterAttack: 6,
-        hasLost: false,
         isEnemy: false,
         id: "#hultron",
+        hpid: "#uhp",
     };
 
     var thanos = {
@@ -46,25 +43,29 @@ $(document).ready(function () {
         healthPoints: 100,
         baseAttackPower: 10,
         attackPower: 10,
-        baseCounterAttack: 10,
-        hasLost: false,
         isEnemy: false,
         id: "#hthanos",
+        hpid: "#thp",
     };
 
     var players = [ironman, hulk, ultron, thanos];
-
-    var gameOn = false;
-
     var pics = ["hironman", "hhulk", "hultron", "hthanos"];
     var champChosen = false;
     var enemyChosen = false;
+    var champ = "";
+    var enemy = "";
+    var unbeaten = 3;
+    var betweenFoes = false;
+    var showRestart = false;
 
-
-    if (!gameOn && !champChosen) {
-        gameOn = true;
+    function choosePlayers() {
+        if (!showRestart) {
+            $("#btn-restart").hide();
+        } else {
+            $("#btn-restart").show();
+        }
         $(".character").click(function () {
-            if (!champChosen) {
+        if (!champChosen) {
             var chosen = this;
             champChosen = true;
             $.each(players, function (i, val) {
@@ -72,28 +73,80 @@ $(document).ready(function () {
                     $("#foes").prepend($(this.id));
                 } else {
                     players[i].isChampion = true;
+                    champ = players[i];
                 };
             });
             $("#champion").text("Your Chosen Hero");
             $("#opponents").text("Choose Your Opponent!");
-            } else if (champChosen && !enemyChosen) {
-                console.log(champChosen);
-                console.log(this.id);
-                $("#current-foe").prepend($("#" + this.id));
-                enemyChosen = true;
-                var enemy = this.id;
-                $.each(players, function(i, val) {
-                    if (pics[i] = enemy) {
-                        players[i].isEnemy = true;
-                    }
-                })
-                } else {
-                    event.preventDefault();
+        } else if (champChosen && unbeaten > 0) {
+            // console.log(champChosen);
+            // console.log(this.id);
+            $("#current-foe").prepend($("#" + this.id));
+            enemyChosen = true;
+            betweenFoes = false;
+            var localEnemy = this.id;
+            // console.log(this.id);
+            $.each(pics, function (i, val) {
+                // console.log(pics[i]);
+                if (pics[i] === localEnemy) {
+                    // console.log(players[i]);
+                    players[i].isEnemy = true;
+                    enemy = players[i];
                 }
             })
+            $("#opponents").text("Opponents Available");
+            $("#defender").text("Chosen Defender!");
+        } else {
+            event.preventDefault();
+        }
 
-    }
+        })
+    };
+    choosePlayers();
 
+    $("#btn-attack").click(function () {
+        if (betweenFoes) {
+            $("#notify").text("There is no-one to attack! Choose an opponent");
+        } else if (champChosen && enemyChosen) {           
+            enemy.healthPoints = enemy.healthPoints - champ.attackPower;
+            $(enemy.hpid).text(enemy.healthPoints);
+            champ.healthPoints = champ.healthPoints - enemy.attackPower;
+            $(champ.hpid).text(champ.healthPoints);
+            $("#notify").text("You hit " + enemy.name + " for " + champ.attackPower + " points of damage. He hit you for " + enemy.attackPower + " points of damage.");
+            champ.attackPower = champ.attackPower + champ.baseAttackPower;
+            if (champ.healthPoints <= 0) {
+                showRestart = true;
+                $("#notify").text("You lost! Press restart to try again.")
+                return;
+            } else if (enemy.healthPoints <= 0) {
+                unbeaten--;
+                $(enemy.id).hide();
+                $("#notify").text("You beat " + enemy.name + "! Choose another opponent carefully, you have " + champ.healthPoints + " health points remaining!")
+                if (unbeaten === 0) {
+                    showRestart = true;
+                    $("#notify").text("You beat all your opponents! Hit restart to play again.");
+                }
+                betweenFoes = true;
+                choosePlayers();
+            }
+        }
+    })
 
+    $("#btn-restart").click(function () {
+        $.each(players, function(i, val) {
+            players[i].isChampion = false;
+            players[i].healthPoints = players[i].baseHealth;
+            players[i].attackPower = players[i].baseAttackPower;
+            players[i].isEnemy = false;
+            var champChosen = false;
+            var enemyChosen = false;
+            var champ = "";
+            var enemy = "";
+            var unbeaten = 3;
+            location.reload();
+        })
 
     })
+
+})
+
